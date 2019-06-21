133e78f0a3da57d6ff933fdcf26d1c04913f4f23
Eugene Korotkov <37555248+eugeneKorotkov@users.noreply.github.com>
2019-06-21 13:52:55 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -8,7 +8,7 @@ public class Main
         Pair pairSecond = new Pair<String, Integer>("6", 4);
         Pair pairThird = new Pair<Integer, String>(2, "2");
         Pair pairFourth = new Pair<String, Integer>("5", 4);
-
+        //results: false, false, false, true
         System.out.println("results: " + pairFirst.equals(pairSecond) + ", " + pairSecond.equals(pairThird) + ", " + pairFirst.equals(pairThird) + ", " + pairFirst.equals(pairFourth));
     }
 }
